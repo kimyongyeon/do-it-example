f7dce86b141eebffa94010bc2bee32eb614f9adb
김용연 SKCC <jobkim0410@partner.sk.com>
2020-06-25 17:39:48 +0900

    first project

---
+++ src/01/RCC.jsx
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+
+class RCC extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default RCC;
